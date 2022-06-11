283394df293e199046f4008172b13b0f8fe4fd03
Dayane <daymlima12@gmail.com>
2022-06-11 17:04:39 -0300

    inicio projeto jogo da velha

--- Interações DOM COM JS/aula2Don/main.js
+++ Interações DOM COM JS/aula2Don/main.js
@@ -1,16 +1,27 @@
 //Como 
 let boxes =  document.getElementsByClassName('box')
+let cores = ['red','blue','green','pink','yellow' ]
 
 for (let i = 0; i < boxes.length; i++) {
-    let element = boxes[i]
-    let cores = ['red','blue','green','pink','yellow' ]
+        let element = boxes[i]
 
-    function changeColor (){
-        element.style.background = cores[i]
-    }
+        function changeColor (){
+            if (element.style.background === '' ){
+                element.style.background = cores[i]
+            }else{
+                element.style.background = ''
+            }
+            
+            // ou
+            // if(i === 0){
+            //     element.style.background = cores[0]
+            // }
+            // if(i === 1){
+            //     element.style.background = cores[1]
+            // }   
+        }
     
     element.addEventListener('click', changeColor)
-    
 }
 
 
